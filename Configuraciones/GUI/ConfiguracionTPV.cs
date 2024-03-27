1ae9496c6f15ce1c0a976f6d6da094e7d6247360
EdgarFuentes100 <edgarfuentes139@gmail.com>
2024-03-26 22:20:15 -0300

    Se corrigio para poder ingresar doble \\

--- Configuraciones/GUI/ConfiguracionTPV.cs
+++ Configuraciones/GUI/ConfiguracionTPV.cs
@@ -506,37 +506,36 @@ namespace Configuraciones.GUI
 
                 if (cmbComandasTickets.Text != string.Empty)
                 {
-                    config.PrinterComanda = cmbComandasTickets.Text;
+                    config.PrinterComanda = cmbComandasTickets.Text.Replace("\\\\", "\\\\\\\\");
                 }
                 if (cmbInformes.Text != string.Empty)
                 {
-                    config.PrinterInformes = cmbInformes.Text;
+                    config.PrinterInformes = cmbInformes.Text.Replace("\\\\", "\\\\\\\\");
                 }
                 if (cmbFacturas.Text != string.Empty)
                 {
-                    config.PrinterFactura = cmbFacturas.Text;
+                    config.PrinterFactura = cmbFacturas.Text.Replace("\\\\", "\\\\\\\\");
                 }
                 if (cmbAppMovil.Text != string.Empty)
                 {
-                    config.ImpresoraAppMovil = cmbAppMovil.Text;
+                    config.ImpresoraAppMovil = cmbAppMovil.Text.Replace("\\\\", "\\\\\\\\");
                 }
                 if (cmbCocina.Text != string.Empty)
                 {
-                    config.ImpresoraCocina = cmbCocina.Text;
+                    config.ImpresoraCocina = cmbCocina.Text.Replace("\\\\", "\\\\\\\\");
                 }
                 if (cmbBar.Text != string.Empty)
                 {
-                    config.ImpresoraBar = cmbBar.Text;
+                    config.ImpresoraBar = cmbBar.Text.Replace("\\\\", "\\\\\\\\");
                 }
                 if (cmbUno.Text != string.Empty)
                 {
-                    config.ImpresoraGrupoUno = cmbUno.Text;
+                    config.ImpresoraGrupoUno = cmbUno.Text.Replace("\\\\", "\\\\\\\\");
                 }
                 if (cmbDos.Text != string.Empty)
                 {
-                    config.ImpresoraGrupoDos = cmbDos.Text;
+                    config.ImpresoraGrupoDos = cmbDos.Text.Replace("\\\\", "\\\\\\\\");
                 }
-
                 if (config.Actualizar())
                 {
                     //Tambien actualizar valores en las dimensiones
