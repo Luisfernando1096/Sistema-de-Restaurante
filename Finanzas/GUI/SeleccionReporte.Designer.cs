dc8b01a5e76551fa32530b88873016aaa19fe983
EdgarFuentes100 <edgarfuentes139@gmail.com>
2024-04-20 12:18:34 -0300

    Se creo el reporte cierre de caja tamaño ticket, y se agrego un  formulario para pedir el tamaño del reporte.

---
+++ Finanzas/GUI/SeleccionReporte.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace Finanzas.GUI
+{
+    partial class SeleccionReporte
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SeleccionReporte));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bttNo = new System.Windows.Forms.Button();
+            this.bttSi = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnSalir);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(757, 434);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Image = ((System.Drawing.Image)(resources.GetObject("btnSalir.Image")));
+            this.btnSalir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSalir.Location = new System.Drawing.Point(531, 12);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(148, 69);
+            this.btnSalir.TabIndex = 15;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.bttNo);
+            this.panel2.Controls.Add(this.bttSi);
+            this.panel2.Location = new System.Drawing.Point(76, 98);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(603, 238);
+            this.panel2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 13.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(128, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(319, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "¿Que tamaño de reporte desea?";
+            // 
+            // bttNo
+            // 
+            this.bttNo.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttNo.Location = new System.Drawing.Point(329, 117);
+            this.bttNo.Name = "bttNo";
+            this.bttNo.Size = new System.Drawing.Size(135, 50);
+            this.bttNo.TabIndex = 2;
+            this.bttNo.Text = "Tamaño ticket";
+            this.bttNo.UseVisualStyleBackColor = true;
+            this.bttNo.Click += new System.EventHandler(this.bttNo_Click);
+            // 
+            // bttSi
+            // 
+            this.bttSi.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttSi.Location = new System.Drawing.Point(123, 117);
+            this.bttSi.Name = "bttSi";
+            this.bttSi.Size = new System.Drawing.Size(135, 50);
+            this.bttSi.TabIndex = 1;
+            this.bttSi.Text = "Tamaño normal";
+            this.bttSi.UseVisualStyleBackColor = true;
+            this.bttSi.Click += new System.EventHandler(this.bttSi_Click);
+            // 
+            // SeleccionReporte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 434);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SeleccionReporte";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "SeleccionReporte";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bttNo;
+        private System.Windows.Forms.Button bttSi;
+    }
+}
---
+++ Finanzas/GUI/SeleccionReporte.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finanzas.GUI
+{
+    public partial class SeleccionReporte : Form
+    {
+        public int seleccion  = 0;
+        public SeleccionReporte()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void bttSi_Click(object sender, EventArgs e)
+        {
+            seleccion = 1;
+            Close();
+        }
+
+        private void bttNo_Click(object sender, EventArgs e)
+        {
+            seleccion = 2;
+            Close();
+        }
+    }
+}
